4cfe291aec0d52d9ed7a78e28a4da03dbb70ccd2
Lautaro <lautaro.dolberg@gmail.com>
2021-12-04 19:42:10 +0100

    day 3

--- day3.clj
+++ day3.clj
@@ -6,19 +6,70 @@
   (map read-string (str/split in #""))
 )
 
-
-(defn add-vec [y x]
-  (loop [a x b y res []]
-    (let [[xi & x_tail] a [yi & y_tail] b]
-      (if (empty? x_tail)
-        res
-        (recur x_tail y_tail (conj res (+ xi yi)))))))
-
-(defn resolve [x]
-  (map #(if (> (/ nlines 2) %) 1 0) x)
+(defn find_most_common_based_on_first_digit [k elems]
+  (let [freq (reduce
+      (fn [a b]
+        (println a b (nth b k))
+        (if (= (nth b k) 0) 
+          {:zero (inc (:zero a)) :one (:one a)}
+          {:zero (:zero a) :one (inc (:one a))}))
+    {:zero 0 :one 0}
+    elems)])   
 )
 
-(->>
-input
-(map parse_line)
-(reduce add-vec)
+(defn most_frequent_key_by_pos [x n] 
+  (key (apply max-key val (frequencies (map #(nth % n) x)))))
+
+(defn least_frequent_key_by_pos [x n] 
+  (key (apply min-key val (frequencies (map #(nth % n) x)))))
+
+(def a
+  (Integer/parseInt (->> (range 12)
+    (map #(most_frequent_key_by_pos (map parse_line input) %))
+    (reduce conj [])
+    (str/join "")) 2))
+
+(def b 
+  (Integer/parseInt (->> (range 12)
+    (map #(least_frequent_key_by_pos (map parse_line input) %))
+    (reduce conj [])
+    (str/join "")) 2))
+
+
+
+(println "Part 1")
+(println (* a b))
+
+(defn filter-most-frequents-by-index [k elems]
+  (let [mf (most_frequent_key_by_pos elems k)]
+  (filter 
+    #(= (nth % k) mf)
+    elems )))
+
+(defn filter-least-frequents-by-index [k elems]
+  (let [mf (least_frequent_key_by_pos elems k)]
+  (filter 
+    #(= (nth % k) mf)
+    elems )))
+
+
+(def a2
+     (loop [elems (map parse_line input) idx 0]
+      (if (= idx 12)
+        elems
+        (recur (filter-most-frequents-by-index idx elems) (inc idx)))))
+
+
+(def b2
+  (loop [elems (map parse_line input) idx 0]
+    (if (= idx 12)
+      elems
+      (recur (filter-least-frequents-by-index idx elems) (inc idx)))))
+
+(println "Part 2")
+(println 
+  (reduce * 
+    (map #(Integer/parseInt % 2) 
+      (map #(str/join "" %) 
+        (map first 
+          [a2 b2])))))
